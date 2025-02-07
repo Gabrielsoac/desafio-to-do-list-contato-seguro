ae1014dd71ebb7a4eb274d92116139890456c9cb
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-07 10:30:17 -0300

    feat: added mongodbUserRepository with create user method

---
+++ src/controllers/user/CreateUserController.ts
@@ -0,0 +1,20 @@
+// import { Request, Response } from "express";
+// import { TCreateUserRequestDto } from "../../usecases/user/create/TCreateUserRequestDto";
+// import { TUserResponseDto } from "../../usecases/user/TUserResponseDto";
+// import { StatusCodes } from "http-status-codes";
+// import { CreateUser } from "../../usecases/user/create/CreateUser";
+
+// // const userRepository = 
+// // const mongodb = CreateUser.create(); 
+
+// // export const CreateUserController = (
+// //     req: Request<TCreateUserRequestDto>,
+// //     res: Response<TUserResponseDto>) => {
+
+// //     try {
+
+// //         res.status(StatusCodes.OK).json(user);
+// //     }
+
+
+// // }
---
+++ src/repository/user/MongodbUserRepository.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { User } from "../../domain/entities/user/User";
+import { UserModel } from "../../models/UserModel";
+import { TUpdateUserRequestDto } from "../../usecases/user/update/TUpdateUserRequestDto";
+import { IUserRepository } from "./IUserRepository";
+import { TPersistedUser } from "./TPersistedUser";
+
+export class MongodbUserRepository implements IUserRepository {
+    
+    public async createUser(user: User): Promise<TPersistedUser> {
+        
+        try {
+            const userPersisted = await UserModel.create(user);
+
+            return {
+                id: userPersisted._id.toString(),
+                name: userPersisted.name,
+                email: userPersisted.email
+            };
+        } catch(err) {
+            throw new Error(`Erro ao salvar usuário: ${(err as Error).message}`);
+        }
+        
+    }
+    
+    public async findById(id: string): Promise<TPersistedUser> {
+        throw new Error("Method not implemented.");
+    }
+    public async findAll(): Promise<TPersistedUser[]> {
+        throw new Error("Method not implemented.");
+    }
+    
+    public async updateUser(userUpdateData: TUpdateUserRequestDto): Promise<TPersistedUser> {
+        throw new Error("Method not implemented.");
+    }
+    
+    public async deleteUser(id: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+}
