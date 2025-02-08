1f782b66239202d996b8911376c53a86bb351e9d
Gabriel Soares <gabrielsoacc@gmail.com>
2025-02-08 14:27:08 -0300

    refactor: user model for unique email for user

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -4,10 +4,12 @@ import { TUserProps } from "../domain/entities/user/TUserProps";
 const userSchema = new Schema<TUserProps>(
     {
         name: {type: String, required: true},
-        email: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
         password: {type: String, required: true},
     }
 );
+
+userSchema.index({email: 1}, {unique: true});
 
 export const UserModel = model<TUserProps>('User', userSchema);
 
